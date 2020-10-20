d926325bcff5a884c0598c8280faf3febe3ccdbb
shahid-waris <comsian.lahor@gmail.com>
2020-10-20 14:58:21 +0500

    Create new userand save it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_122824) do
+ActiveRecord::Schema.define(version: 2020_10_20_060618) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_122824) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
