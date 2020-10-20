2257e60e1803f477f6c4d8a2f0a1f32d03d8b1cb
shahid-waris <comsian.lahor@gmail.com>
2020-10-20 19:04:27 +0500

    add bloggers and edit articles etc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,19 @@ class UsersController < ApplicationController
   def edit
      @user=User.find(params[:id])
   end
+  def show
+    @user=User.find(params[:id])
+    @articles=@user.articles
+  end
+  def index
+    @users=User.all
+  end 
 
   def update  
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
