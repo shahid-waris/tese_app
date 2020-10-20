644329938aaad595b8cc737fef7c19bb6e50eadc
shahid-waris <comsian.lahor@gmail.com>
2020-10-20 15:12:36 +0500

    edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def edit
+
+  end
+
+  def update  
+
+    
+  end
 
   def create
     @user = User.new(user_params)
