2285eab4a61e666ea5e1db2d8a451bc36d33ee7c
shahid-waris <comsian.lahor@gmail.com>
2021-01-10 20:47:22 +0500

    add tailwind

--- db/schema.rb
+++ db/schema.rb
@@ -22,9 +22,9 @@ ActiveRecord::Schema.define(version: 2020_10_20_060618) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "password_digest"
   end
 
